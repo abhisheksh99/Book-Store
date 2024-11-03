8515045acdce37bbe2c9ee75fa84aee49a1f638c
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-03 09:42:03 -0500

    Minor changes made to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import MainLayout from './components/MainLayout';
-import AuthLayout from './components/AuthLayout';
+
 
 
 function App() {
@@ -14,10 +14,6 @@ function App() {
         {/* Routes with MainLayout (Navbar and Footer) */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-        </Route>
-
-        {/* Routes with AuthLayout (no Navbar or Footer) */}
-        <Route element={<AuthLayout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Route>
