cc06288eed7efa608bf645771d23a98f49f215a4
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-02 21:15:52 -0400

    Navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
-    <>
-    <h1>App</h1>
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+
+
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiMiniBars3CenterLeft } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa6";
+import { HiMiniShoppingCart } from "react-icons/hi2";
+import avatarImg from "../assets/avatar.png";
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Orders",
+    href: "/orders",
+  },
+  {
+    name: "Cart Page",
+    href: "/cart",
+  },
+  {
+    name: "Check Out",
+    href: "/checkout",
+  },
+];
+
+const Navbar = () => {
+  const currentUser = true;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        {/* Left Side */}
+        <div className="flex items-center justify-center md:gap-16 gap-4">
+          <Link to="/">
+            <HiMiniBars3CenterLeft className="size-6" />
+          </Link>
+
+          {/* Search Input */}
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/* Right Side */}
+        <div className="flex relative items-center md:space-x-3 space-x-2">
+          <div>
+            {currentUser ? (
+              <>
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  <img
+                    src={avatarImg}
+                    alt="Logged in user"
+                    className={`size-7 rounded-full ${currentUser ? "ring-2 ring-blue-500" : ""}`}
+                  />
+                </button>
+                {/* Dropdown */}
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
+                      {navigation.map((item) => (
+                        <li key={item.name} onClick={()=>setIsDropdownOpen(false)}>
+                          <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-200">
+                            {item.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Link to="/login">
+                <FaRegUser className="size-6" />
+              </Link>
+            )}
+          </div>
+          <button className="hidden sm:block">
+            <FaRegHeart className="size-6" />
+          </button>
+          <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-md">
+            <HiMiniShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
