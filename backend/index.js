21f11f3d0b71a639128c899cd385796cf9e5f3af
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-03 11:47:03 -0500

    Server running and db connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDb from "./config/Db.js"
 
 dotenv.config()
 
 const app = express()
+connectDb()
 
 
 app.listen(process.env.PORT, ()=>{
